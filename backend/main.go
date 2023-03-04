0d78f4e69d1a01af3141405ecc7b1f4a174ae5bb
Sora <23435312+soramon0@users.noreply.github.com>
2023-03-04 11:55:25 +0100

    server tours-simple.json

--- backend/main.go
+++ backend/main.go
@@ -1,20 +1,50 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 type Tour struct {
-	Name string
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+type APIResponse struct {
+	Data  interface{} `json:"data"`
+	Count int         `json:"count"`
+	Error string      `json:"error"`
+}
+
+func readJsonData(filename string) *[]Tour {
+	data, err := os.ReadFile(fmt.Sprintf("./data/%s.json", filename))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var tours []Tour
+
+	if err = json.Unmarshal(data, &tours); err != nil {
+		log.Fatalln(err)
+	}
+
+	return &tours
 }
 
 func main() {
+	tours := readJsonData("tours-simple")
 	app := fiber.New()
+	toursApi := app.Group("/api/v1/tours")
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.JSON(Tour{Name: "Hello"})
+	toursApi.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(APIResponse{Data: tours, Count: len(*tours)})
+	})
+	toursApi.Post("/", func(c *fiber.Ctx) error {
+		return c.JSON(APIResponse{Error: fiber.ErrNotImplemented.Message})
 	})
 
 	log.Fatalln(app.Listen(":3000"))
