f689f7a463e4e274b1cc7742b2e0a59b87c49436
soramon0 <23435312+soramon0@users.noreply.github.com>
2023-03-04 15:39:48 +0100

    feat: get single tour, add error handler

--- backend/main.go
+++ backend/main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 )
 
 type Tour struct {
@@ -32,7 +36,8 @@ type APIResponse struct {
 }
 
 type APIError struct {
-	Message string `json:"message"`
+	Message    string `json:"message"`
+	statusCode int
 }
 
 func readJsonFile(filename string, data any) error {
@@ -54,16 +59,70 @@ func writeJsonFile(filename string, data any) error {
 }
 
 func main() {
-	app := fiber.New()
+	app := fiber.New(fiber.Config{
+		ErrorHandler: func(ctx *fiber.Ctx, err error) error {
+			// Status code and message defaults to 500
+			apiError := APIResponse{Error: &APIError{
+				Message:    fiber.ErrInternalServerError.Error(),
+				statusCode: fiber.StatusInternalServerError,
+			}}
+
+			// Retrieve the custom status code if it's a *fiber.Error
+			var e *fiber.Error
+			if errors.As(err, &e) {
+				apiError.Error.statusCode = e.Code
+				apiError.Error.Message = e.Message
+			}
+
+			// Send custom error response
+			if err := ctx.Status(apiError.Error.statusCode).JSON(apiError); err != nil {
+				return ctx.Status(fiber.StatusInternalServerError).JSON(APIResponse{Error: &APIError{
+					Message: "Internal Server Error",
+				}})
+			}
+
+			// Return from handler
+			return nil
+		},
+	})
+	app.Use(recover.New())
+	app.Use(logger.New())
+
 	toursApi := app.Group("/api/v1/tours")
 
 	toursApi.Get("/", func(c *fiber.Ctx) error {
 		var tours []Tour
 		if err := readJsonFile("tours-simple", &tours); err != nil {
-			return c.Status(fiber.StatusInternalServerError).JSON(APIResponse{Error: &APIError{Message: err.Error()}})
+			return &fiber.Error{Code: fiber.StatusInternalServerError, Message: err.Error()}
 		}
 
 		return c.JSON(APIResponse{Data: tours, Count: len(tours)})
+	})
+
+	toursApi.Get("/:id", func(c *fiber.Ctx) error {
+		id, err := strconv.Atoi(c.Params("id"))
+		if err != nil {
+			return &fiber.Error{Code: fiber.StatusBadRequest, Message: "Invalid Id"}
+		}
+
+		var tours []Tour
+		if err := readJsonFile("tours-simple", &tours); err != nil {
+			return &fiber.Error{Code: fiber.StatusInternalServerError, Message: err.Error()}
+		}
+
+		var tour *Tour
+		for _, item := range tours {
+			if item.Id == id {
+				tour = &item
+				break
+			}
+		}
+
+		if tour == nil {
+			return &fiber.Error{Code: fiber.StatusNotFound, Message: "Tour not found"}
+		}
+
+		return c.JSON(APIResponse{Data: tour, Count: 1})
 	})
 
 	toursApi.Post("/", func(c *fiber.Ctx) error {
@@ -72,25 +131,25 @@ func main() {
 		}{}
 
 		if err := c.BodyParser(&payload); err != nil {
-			return c.Status(fiber.StatusBadRequest).JSON(APIResponse{Error: &APIError{Message: err.Error()}})
+			return &fiber.Error{Code: fiber.StatusBadRequest, Message: err.Error()}
 		}
 
 		if payload.Name == "" {
-			return c.Status(fiber.StatusBadRequest).JSON(APIResponse{Error: &APIError{Message: "Name is required"}})
+			return &fiber.Error{Code: fiber.StatusBadRequest, Message: "Name is required"}
 		}
 
 		var tours []Tour
 		if err := readJsonFile("tours-simple", &tours); err != nil {
-			return c.Status(fiber.StatusInternalServerError).JSON(APIResponse{Error: &APIError{Message: err.Error()}})
+			return &fiber.Error{Code: fiber.StatusInternalServerError, Message: err.Error()}
 		}
 
 		tour := Tour{Name: payload.Name, Id: len(tours)}
 		tours = append(tours, tour)
 		if err := writeJsonFile("tours-simple", &tours); err != nil {
-			return c.Status(fiber.StatusInternalServerError).JSON(APIResponse{Error: &APIError{Message: err.Error()}})
+			return &fiber.Error{Code: fiber.StatusInternalServerError, Message: err.Error()}
 		}
 
-		return c.Status(fiber.StatusCreated).JSON(APIResponse{Data: tour})
+		return c.Status(fiber.StatusCreated).JSON(APIResponse{Data: tour, Count: 1})
 	})
 
 	log.Fatalln(app.Listen(":5000"))
