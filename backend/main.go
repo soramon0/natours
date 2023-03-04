c1c95d9e36f9a6eed45660814ba7f2912a0be83a
soramon0 <23435312+soramon0@users.noreply.github.com>
2023-03-04 14:56:13 +0100

    feat: tours read/write json file endpoints

--- backend/main.go
+++ backend/main.go
@@ -10,8 +10,19 @@ import (
 )
 
 type Tour struct {
-	Id   int    `json:"id"`
-	Name string `json:"name"`
+	Id              int      `json:"id"`
+	Name            string   `json:"name"`
+	Duration        int      `json:"duration"`
+	MaxGroupSize    int      `json:"maxGroupSize"`
+	Difficulty      string   `json:"difficulty"`
+	RatingsAverage  float64  `json:"ratingsAverage"`
+	RatingsQuantity int      `json:"ratingsQuantity"`
+	Price           int      `json:"price"`
+	Summary         string   `json:"summary"`
+	Description     string   `json:"description"`
+	ImageCover      string   `json:"imageCover"`
+	Images          []string `json:"images"`
+	StartDates      []string `json:"startDates"`
 }
 
 type APIResponse struct {
@@ -24,31 +35,62 @@ type APIError struct {
 	Message string `json:"message"`
 }
 
-func readJsonData(filename string) *[]Tour {
-	data, err := os.ReadFile(fmt.Sprintf("./data/%s.json", filename))
+func readJsonFile(filename string, data any) error {
+	jsonFile, err := os.ReadFile(fmt.Sprintf("./data/%s.json", filename))
 	if err != nil {
-		log.Fatalln(err)
+		return err
 	}
 
-	var tours []Tour
+	return json.Unmarshal(jsonFile, &data)
+}
 
-	if err = json.Unmarshal(data, &tours); err != nil {
-		log.Fatalln(err)
+func writeJsonFile(filename string, data any) error {
+	file, err := json.MarshalIndent(data, "", " ")
+	if err != nil {
+		return err
 	}
 
-	return &tours
+	return os.WriteFile(fmt.Sprintf("./data/%s.json", filename), file, 0644)
 }
 
 func main() {
-	tours := readJsonData("tours-simple")
 	app := fiber.New()
 	toursApi := app.Group("/api/v1/tours")
 
 	toursApi.Get("/", func(c *fiber.Ctx) error {
-		return c.JSON(APIResponse{Data: tours, Count: len(*tours)})
+		var tours []Tour
+		if err := readJsonFile("tours-simple", &tours); err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(APIResponse{Error: &APIError{Message: err.Error()}})
+		}
+
+		return c.JSON(APIResponse{Data: tours, Count: len(tours)})
 	})
+
 	toursApi.Post("/", func(c *fiber.Ctx) error {
-		return c.JSON(APIResponse{Error: &APIError{Message: fiber.ErrNotImplemented.Message}})
+		payload := struct {
+			Name string `json:"name"`
+		}{}
+
+		if err := c.BodyParser(&payload); err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(APIResponse{Error: &APIError{Message: err.Error()}})
+		}
+
+		if payload.Name == "" {
+			return c.Status(fiber.StatusBadRequest).JSON(APIResponse{Error: &APIError{Message: "Name is required"}})
+		}
+
+		var tours []Tour
+		if err := readJsonFile("tours-simple", &tours); err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(APIResponse{Error: &APIError{Message: err.Error()}})
+		}
+
+		tour := Tour{Name: payload.Name, Id: len(tours)}
+		tours = append(tours, tour)
+		if err := writeJsonFile("tours-simple", &tours); err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(APIResponse{Error: &APIError{Message: err.Error()}})
+		}
+
+		return c.Status(fiber.StatusCreated).JSON(APIResponse{Data: tour})
 	})
 
 	log.Fatalln(app.Listen(":5000"))
